76463d196e8698ce882f8233bf23b3595fdb179b
sqfasd <sqf1225@foxmail.com>
2017-07-28 19:07:50 +0800

    use tweetnacl instead of js-nacl

--- test.js
+++
@@ -1,4 +0,0 @@
-var asch = require('./index.js')
-
-var keys = asch.crypto.getKeys('divide cheap guilt mirror soldier citizen fox absorb perfect emotion profit despair')
-console.log(asch.crypto.getAddress(keys.publicKey))
